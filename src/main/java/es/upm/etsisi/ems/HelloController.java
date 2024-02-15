0985b66f0551c9ce82d6c439df678d44a01b0ae2
fdcogollo <159068856+fdcogollo@users.noreply.github.com>
2024-02-15 09:26:18 +0100

    Update HelloController.java

--- src/main/java/es/upm/etsisi/ems/HelloController.java
+++ src/main/java/es/upm/etsisi/ems/HelloController.java
@@ -19,7 +19,7 @@ public class HelloController {
     }
 
     @RequestMapping("/hola")
-    public String hola(@RequestParam(value = "nombre", required = false, defaultValue = "Mundo") String nombre, Model model) {
+    public String hola(@RequestParam(value = "nombre", required = false, defaultValue = "Mundo") String nombre1, Model model) {
         model.addAttribute("nombre", nombre);
         return "hola";
     }
